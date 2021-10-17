1c91078109d07696d9287c9ab38604fb02a80f3c
Bartosz Przydatek <bartoszprzydatek1995@gmail.com>
2021-10-17 22:12:35 +0200

    Operation model added Database context updated with Operation model

--- OperationsService/Data/DatabaseContext.cs
+++ OperationsService/Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OperationsService.Data.Models;
 
 namespace OperationsService.Data
 {
@@ -7,6 +8,24 @@ namespace OperationsService.Data
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
             
+        }
+
+        public DbSet<Operation> Operations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Operation>()
+                .ToTable("Operations");
+
+            modelBuilder.Entity<Operation>()
+                .Property(c => c.Bank)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Operation>()
+                .Property(c => c.Type)
+                .HasConversion<string>();
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ OperationsService/Data/Models/Operation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OperationsService.Data.Models
+{
+    public class Operation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+        
+        [Required] 
+        public Double Amount { get; set; }
+        
+        [Required] 
+        public String Description { get; set; }
+        
+        [Required] 
+        public BankEnum Bank { get; set; }
+        
+        [Required] 
+        public OperationType Type { get; set; }
+
+        public enum BankEnum
+        {
+            PkoBp
+        }
+
+        public enum OperationType
+        {
+            IncomeTransfer, 
+            OutgoingTransfer, 
+            DebitCardPayment, 
+            MobileCodePayment, 
+            CashWithdrawal, 
+            Commission, 
+            NotResolved, 
+            Refund, 
+            StandingOrder, 
+            NotApplicable, 
+            NotFound
+        }
+    }
+}
